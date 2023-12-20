1513e4532e374db0b7a6bd05719432693c033d23
Vinu <vinujithmin@gmail.com>
2023-12-20 13:50:55 +1100

    add christmas feature

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Tasker from './features/Tasker'
+import { createRoot } from 'react-dom/client'
+import Tasker from '@features/Tasker'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Christmas } from '@features/Christmas'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 root.render(
   <div>
     <Provider store={store}>
-      <Tasker />
+      {/* <Tasker /> */}
+      <Christmas />
     </Provider>
   </div>
 )
